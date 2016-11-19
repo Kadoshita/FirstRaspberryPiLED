c6ae7fad49e9aa0cb736324c27cb3724a8c9d82d
Yoshiki Kadoshita <kadoshita.inct@gmail.com>
2016-11-19 19:04:56 +0900

    Create led.py

---
+++ led.py
@@ -0,0 +1,30 @@
+
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+while True:
+    GPIO.output(11, True)
+    GPIO.output(13, False)
+    GPIO.output(15, False)
+    GPIO.output(16, False)
+    time.sleep(0.1)
+    GPIO.output(13, True)
+    GPIO.output(11, False)
+    GPIO.output(15, False)
+    GPIO.output(16, False)
+    time.sleep(0.1)
+    GPIO.output(15, True)
+    GPIO.output(11, False)
+    GPIO.output(13, False)
+    GPIO.output(16, False)
+    time.sleep(0.1)
+    GPIO.output(16, True)
+    GPIO.output(11, False)
+    GPIO.output(13, False)
+    GPIO.output(15, False)
+    time.sleep(0.1)
